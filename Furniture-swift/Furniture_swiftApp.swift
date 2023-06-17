6ec508605895815200f1678e73eda41cb1976604
Kemal Maulana <kemalmaulana19@gmail.com>
2023-06-18 01:26:49 +0700

    Initial Commit

---
+++ Furniture-swift/Furniture_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Furniture_swiftApp.swift
+//  Furniture-swift
+//
+//  Created by Kemal Maulana on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Furniture_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
